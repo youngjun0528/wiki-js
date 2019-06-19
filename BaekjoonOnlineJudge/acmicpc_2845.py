2cb98239806128b1f1bd6f6b49c1cce6fe3ff942
youngjun0528 <youngjun0528@gmail.com>
2019-06-19 14:32:05 +0900

    Create acmicpc_2845.py

---
+++ BaekjoonOnlineJudge/acmicpc_2845.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# 파티가 끝나고 난 뒤
+# https://www.acmicpc.net/problem/2845
+
+import sys
+
+if __name__ == '__main__':
+    num_arr = sys.stdin.readline().strip().split(' ')
+    inputData_arr = sys.stdin.readline().strip().split(' ')
+    member = int(num_arr[0]) * int(num_arr[1])
+    result = ''
+    for data in inputData_arr:
+        result += str(int(data) - member) + ' '
+    print(result)
