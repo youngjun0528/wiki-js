3540f84d6874790e05dcac069d651f5302e17c31
youngjun0528 <youngjun0528@gmail.com>
2019-06-19 14:03:38 +0900

    Create acmicpc_2941.py

---
+++ BaekjoonOnlineJudge/acmicpc_2941.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding=utf-8 -*-
+
+# 크로아티아 알파벳
+# https://www.acmicpc.net/problem/2941
+
+def main(inputData):
+    check = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+    cnt = 0
+    for c in check:
+        if c in inputData:
+            cnt += 1
+            inputData = inputData.replace(c, '0')
+    print(len(inputData))
+
+import sys
+if __name__ == '__main__':
+    inputData = sys.stdin.readline().strip()
+    main(inputData)
